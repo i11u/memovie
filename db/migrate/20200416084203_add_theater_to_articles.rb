2c29003dfa0d35431ebc8d0a5ab8a3f3f89adefe
Teru <incredible@gmail.com>
2020-04-18 13:34:52 +0900

    Initialized

--- db/migrate/20200416084203_add_theater_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddTheaterToArticles < ActiveRecord::Migration[6.0]
-  def change
-    add_column :articles, :theater, :string
-  end
-end
--- db/migrate/20200416094001_add_date_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddDateToArticles < ActiveRecord::Migration[6.0]
-  def change
-    add_column :articles, :watched_date, :datetime
-  end
-end
