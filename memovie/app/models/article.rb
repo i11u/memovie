c15fad98eefbaa11d81b9ee277d654c365680077
Teru <incredible@gmail.com>
2020-04-28 20:54:49 +0900

    article_test has been modified

--- memovie/app/models/article.rb
+++ memovie/app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    validates :title, presence: true
+
     def self.search(search)
         if search
             Article.where(['title LIKE ? OR theater LIKE ? OR watched_date LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%"])
