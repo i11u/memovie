426af5c03844ea5745052dce9ef77b098bd6d31d
Teru <incredible@gmail.com>
2020-04-28 15:56:19 +0900

    Theater/watched_date search enabled

--- memovie/app/models/article.rb
+++ memovie/app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
     def self.search(search)
         if search
-            Article.where(['title LIKE ?', "%#{search}%"])
+            Article.where(['title LIKE ? OR theater LIKE ? OR watched_date LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%"])
         else 
             Article.all
         end
