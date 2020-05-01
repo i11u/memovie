da2c97dfad250e45231b7d25590b2f3fd40272d7
Teru <incredible@gmail.com>
2020-05-01 14:13:57 +0900

    System tests are added

--- memovie/test/integration/memovie_flow_test.rb
+++ memovie/test/integration/memovie_flow_test.rb
@@ -5,7 +5,7 @@ class MemovieFlowTest < ActionDispatch::IntegrationTest
     get "/articles/new"
     assert_response :success
 
-    post "/articles", params: { article: { title: "can create", body: "article successfully.", watched_date: "2018/07/16 12:00:00 +09:00"} }
+    post "/articles", params: { article: { title: "can create", body: "article successfully.", watched_date: "2018/07/16 12:00:00 +09:00", rate: 4 } }
     assert_response :redirect
     follow_redirect!
     assert_response :success
